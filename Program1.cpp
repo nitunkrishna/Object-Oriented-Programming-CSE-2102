b065eb023dfb29c10e970e6b7c95a80d89a3de62
Nitun Krishna Biswas <tgnitun05@gmail.com>
2025-10-12 19:22:51 +0600

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,35 @@
+// 12 October, 2025
+
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+class student
+{
+    public:
+    string name;
+    int id;
+
+public:
+    void getvalue()
+    {
+        cout << "Enter Student Name: ";
+        cin >> name;
+        cout << "Enter Student Id: ";
+        cin >> id;
+    }
+
+    void displayvalue()
+    {
+        cout << "Student Name: " << name << endl;
+        cout << "Student Id: " << id << endl;
+    }
+};
+
+int main()
+{
+    student s1; //Creation of Object
+    s1.getvalue(); //Getting the value of variables using a function
+    s1.displayvalue();
+}
